f26adfd5a1cf7df612eb8cd0fcf43c446217329b
Fcordier7921 <f.cordier@codeur.online>
2020-10-27 15:42:23 +0100

    avant phase2

--- Models/functions.php
+++ Models/functions.php
@@ -1,9 +1,10 @@
 <?php
-    
+    function conect()
+    {
         try
         {
             $bdd = new PDO('mysql:host=localhost;dbname=hertz;port=3308;charset=utf8', 'root', '');
-         
+            return $bdd;
         }
         catch(Exception $e)
         {
@@ -15,19 +16,16 @@
         
 
         
-   
+    }
     
     function ajouterv()
     {
-
+        $bdd=conect()
         if(isset($_GET['ajouter'])){
             $ajouter = $bdd->prepare('INSERT INTO véhicules (type_Véhicules, modele_Véhicules, immatriculation_Véhicules) VALUES (:type_Véhicules, :modele_Véhicules, :immatriculation_Véhicules)');
-            $ajouter->bindParam(':type_Véhicules', $_GET['type'], 
-            PDO::PARAM_STR);
-            $ajouter->bindParam(':modele_Véhicules', $_GET['modele'], 
-            PDO::PARAM_STR);
-            $ajouter->bindParam(':immatriculation_Véhicules', $_GET['immat'], 
-            PDO::PARAM_STR);
+            $ajouter->bindParam(':type_Véhicules', $_GET['type'],PDO::PARAM_STR);
+            $ajouter->bindParam(':modele_Véhicules', $_GET['modele'],PDO::PARAM_STR);
+            $ajouter->bindParam(':immatriculation_Véhicules', $_GET['immat'],PDO::PARAM_STR);
             $estceok = $ajouter->execute();
         
                 if($estceok){
@@ -152,20 +150,20 @@
     //             }
     //         }
     // }
-    // function aff_voiture() {
-    //     $recuperation = $bdd->query('SELECT * FROM Livre');
-    //     while ($voit = $recuperation->fetch()) {
-    //     echo "<form><div> <input type='text' name='id' value='".$voit['ID_livre']."'>
-    //     <input type='text' name='titredulivre' value='".$voit['auteur_livre']."'>
-    //     <input type='text' name='annee' value='".$voit['non_du_livre_livre']."'>
-    //     <input type='text' name='auteurdulivre' value='".$voit['annee_livre']."'>
+    function aff_voiture() {
+        $recuperation = $bdd->query('SELECT * FROM Livre');
+        while ($voit = $recuperation->fetch()) {
+        echo "<form><div> <input type='text' name='id' value='".$voit['ID_livre']."'>
+        <input type='text' name='titredulivre' value='".$voit['auteur_livre']."'>
+        <input type='text' name='annee' value='".$voit['non_du_livre_livre']."'>
+        <input type='text' name='auteurdulivre' value='".$voit['annee_livre']."'>
         
-    //     <button type='submit' value='modifier2' name='action'>Modifier</button>
-    //     <button type='submit' value='supprimer' name='action'>Supprimer</button>
+        <button type='submit' value='modifier2' name='action'>Modifier</button>
+        <button type='submit' value='supprimer' name='action'>Supprimer</button>
         
-    //     </form>
+        </form>
         
-    //     </div>";
+        </div>";
 
     // }
     // function aff_client() {
