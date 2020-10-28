3798822a6eec43a0abaa8dfc2b9934f35e0268ec
Fcordier7921 <f.cordier@codeur.online>
2020-10-28 11:53:29 +0100

    ajout table client

--- Models/functions.php
+++ Models/functions.php
@@ -1,61 +1,54 @@
 <?php
-    function conect()
+    function connect()
     {
         try
         {
-            $bdd = new PDO('mysql:host=localhost;dbname=hertz;port=3308;charset=utf8', 'root', '');
+            $bdd = new PDO('mysql:host=localhost;dbname=hertz;port=3306;charset=utf8', 'root', '');
             return $bdd;
+         
         }
         catch(Exception $e)
         {
             die('Erreur : '.$e->getMessage());
         }
-        
-
-
-        
-
-        
     }
+        
+
+
+        
+
+        
+   
     
     function ajouterv()
     {
-        $bdd=conect();
-
+        $bdd=connect();
         if(isset($_GET['ajouter'])){
-            $ajouter = $bdd->prepare('INSERT INTO véhicules (type_Véhicules, modele_Véhicules, immatriculation_Véhicules) VALUES (:type_Véhicules, :modele_Véhicules, :immatriculation_Véhicules)');
-            $ajouter->bindParam(':type_Véhicules', $_GET['type'],PDO::PARAM_STR);
-            $ajouter->bindParam(':modele_Véhicules', $_GET['modele'],PDO::PARAM_STR);
-            $ajouter->bindParam(':immatriculation_Véhicules', $_GET['immat'],PDO::PARAM_STR);
-            $estceok = $ajouter->execute();
-        
+            $ajouter = $bdd->prepare('INSERT INTO vehicules ( type_Vehicules, modele_Vehicules, immatriculation_Vehicules) 
+            VALUES (:type_Vehicules, :modele_Vehicules, :immatriculation_Vehicules)');
+            $ajouter->bindParam(':type_Vehicules', $_GET['type'],PDO::PARAM_STR);
+            $ajouter->bindParam(':modele_Vehicules', $_GET['modele'],PDO::PARAM_STR);
+            $ajouter->bindParam(':immatriculation_Vehicules', $_GET['immat'],PDO::PARAM_STR);
+            $estceok=$ajouter->execute();
+            
                 if($estceok){
-                    echo 'votre enregistrement a été ajouté avec succés';
-                    
-                
+                    echo 'votre enregistrement a été ajouté avec succès <br>';
                 } else {
-                    echo 'Veuillez recommencer svp, une erreur est survenue';
+                    echo 'Veuillez recommencer svp, une erreur est survenue <br>';
                 }
-            }
-
     }
     function ajouterc()
     {
-
-        if(isset($_GET['action']) && !empty($_GET['Nom_Clients'])  && !empty($_GET['Prenom_Clients'])  && !empty($_GET['adresse_Clients']) && !empty($_GET['CP_Clients']) && !empty($_GET['Ville_Clients'])){
+        $bdd=connect();
+        if(isset($_GET['action']) && !empty($_GET['nom'])  && !empty($_GET['prenom'])  && !empty($_GET['adresse']) && !empty($_GET['cp']) && !empty($_GET['ville'])){
             $toto =  $_GET['date'];
             $ajouter2 = $bdd->prepare('INSERT INTO clients (Nom_Clients, Prenom_Clients, adresse_Clients, CP_Clients, Ville_Clients  ) VALUES (:Nom_Clients, :Prenom_Clients, :adresse_Clients, :CP_Clients, :Ville_Clients)');
-            $ajouter2->bindParam(':Nom_Clients', $_GET['nom'], 
-            PDO::PARAM_STR);
-            $ajouter2->bindParam(':Prenom_Clients', $_GET['Prenom'], 
-            PDO::PARAM_STR);
-            $ajouter2->bindParam(':adresse_Clients', $_GET['adresse'], 
-            PDO::PARAM_STR);
-            $ajouter2->bindParam(':CP_Clients', $_GET['cp'], 
-            PDO::PARAM_STR);
-            $ajouter2->bindParam(':Ville_Clients', $_GET['Ville_Clients'], 
-            PDO::PARAM_STR);
-            $estceok = $ajouter->execute();
+            $ajouter2->bindParam(':Nom_Clients', $_GET['nom'],PDO::PARAM_STR);
+            $ajouter2->bindParam(':Prenom_Clients', $_GET['prenom'],PDO::PARAM_STR);
+            $ajouter2->bindParam(':adresse_Clients', $_GET['adresse'],PDO::PARAM_STR);
+            $ajouter2->bindParam(':CP_Clients', $_GET['cp'],PDO::PARAM_STR);
+            $ajouter2->bindParam(':Ville_Clients', $_GET['ville'],PDO::PARAM_STR);
+            $estceok = $ajouter2->execute();
         
                 if($estceok){
                     echo 'votre enregistrement a été ajouté avec succés';
@@ -67,57 +60,61 @@
             }
 
     }
-    // function modifiv() 
-    // {
+    function modifiv() 
+    {
+        $bdd=connect();
+        if(isset($_GET['action']) && $_GET['action']=="modifier"  && !empty($_GET['id'])  && !empty($_GET['type'])  && !empty($_GET['modele']) && !empty($_GET['immat'])){
+            $message = '';
+            $modifierv2 = $bdd->prepare('UPDATE vehicules SET id_Vehicules = :id_Vehicules, type_Vehicules = :type_Vehicules, modele_Vehicules = :modele_Vehicules, immatriculation_Vehicules = :immatriculation WHERE id_Vehicules =:id_Vehicules');
+            $modifierv2->bindParam(':id_Vehicules', $_GET['id'], PDO::PARAM_STR);
+            $modifierv2->bindParam(':type_Vehicules', $_GET['type'], PDO::PARAM_STR);
+            $modifierv2->bindParam(':modele_Vehicules', $_GET['modele'], PDO::PARAM_STR);
+            $modifierv2->bindParam(':immatriculation', $_GET['immat'], PDO::PARAM_STR);        
+               
+            $modifierv2 = $modifierv2->execute();
+            // $modifier->debugDumpParams();
+            // die;
+                if($modifierv2){
+                    echo 'votre enregistrement a bien été modifié';
+                                                        
+                
+                } else {
+                    echo 'Veuillez recommencer svp, une erreur est survenue';
+                }
+            }
 
-    //     if(isset($_GET['action']) && $_GET['action']=="modifier"  && !empty($_GET['type_Véhicules'])  && !empty($_GET['modele_Véhicules'])  && !empty($_GET['modele_Véhicules'])){
-    //         $message = '';
-    //         $modifier = $bdd->prepare('UPDATE véhicules SET '.$_GET['colonne'].' = :modif WHERE ID_livre_Livres =:id');
-    //         $modifier->bindParam(':id', $_GET['id'], 
-    //         PDO::PARAM_STR);
-    //         $modifier->bindParam(':modif', $_GET['modif'], 
-    //         PDO::PARAM_STR);
-    //         $modifier = $modifier->execute();
+    }
+    function modific() 
+    {
+        $bdd=connect();
+        if(isset($_GET['action']) && $_GET['action']=="modifier"  && !empty($_GET['id'])  && !empty($_GET['nom'])  && !empty($_GET[''])){
+            $message = '';
+            $modifierc2 = $bdd->prepare('UPDATE clients SET id_Clients = :id_Clients, Nom_Clients = :Nom_Clients, Prenom_Clients = :Prenom_Clients, adresse_Clients = :adresse_Clients, CP_Clients = :CP_Clients, Ville_Clients = :Ville_Clients, WHERE id_Clients =:id_Clients');
+            $modifierc2->bindParam(':id_Clients', $_GET['id'],PDO::PARAM_STR);
+            $modifierc2->bindParam(':Nom_Clients', $_GET['nom'],PDO::PARAM_STR);
+            $modifierc2->bindParam(':Prenom_Clients', $_GET['prenom'],PDO::PARAM_STR);
+            $modifierc2->bindParam(':adresse_Clients', $_GET['adresse'],PDO::PARAM_STR);
+            $modifierc2->bindParam(':CP_Clients', $_GET['cp'],PDO::PARAM_STR);
+            $modifierc2->bindParam(':Ville_Clients', $_GET['ville'],PDO::PARAM_STR);
+            $modifierc2 = $modifier2->execute();
 
-    //             if($modifier){
-    //                 echo 'votre enregistrement a bien été modifié';
+                if($modifier2){
+                    echo 'votre enregistrement a bien été modifié';
                     
                 
-    //             } else {
-    //                 echo 'Veuillez recommencer svp, une erreur est survenue';
-    //             }
-    //         }
+                } else {
+                    echo 'Veuillez recommencer svp, une erreur est survenue';
+                }
+            }
 
-    // }
-    // function modific() 
-    // {
-
-    //     if(isset($_GET['action']) && $_GET['action']=="modifier"  && !empty($_GET[''])  && !empty($_GET[''])  && !empty($_GET[''])){
-    //         $message = '';
-    //         $modifier2 = $bdd->prepare('UPDATE livres SET '.$_GET['colonne'].' = :modif WHERE ID_livre_Livres =:id');
-    //         $modifier2->bindParam(':id', $_GET['id'], 
-    //         PDO::PARAM_STR);
-    //         $modifier2->bindParam(':modif', $_GET['modif'], 
-    //         PDO::PARAM_STR);
-    //         $modifier2 = $modifier2->execute();
-
-    //             if($modifier2){
-    //                 echo 'votre enregistrement a bien été modifié';
-                    
-                
-    //             } else {
-    //                 echo 'Veuillez recommencer svp, une erreur est survenue';
-    //             }
-    //         }
-
-    // }
+    }
     function supriv()
     {
-        $bdd=conect();
-        if(isset($_GET['action']) && $_GET['action']=="supprimer" && !empty($_GET['	id'])){
+        $bdd=connect();
+        if(isset($_GET['action']) && $_GET['action']=="supprimer" && !empty($_GET['id'])){
             
             $supprimer = $bdd->prepare('DELETE FROM vehicules WHERE id_Vehicules =:id_Vehicule');
-            $supprimer->bindParam(':id_Vehicules', $_GET['id'],PDO::PARAM_STR);
+            $supprimer->bindParam(':id_Vehicule', $_GET['id'],PDO::PARAM_STR);
 
 
             $supprimer = $supprimer->execute();
@@ -130,57 +127,68 @@
                 }
             }
     }
-    // function supric()
-    // {
-
-    //     if(isset($_GET['action']) && $_GET['action']=="supprimer" && !empty($_GET['id_Clients'])){
+    function supric()
+    {
+        $bdd=connect();
+        if(isset($_GET['action']) && $_GET['action']=="supprimer" && !empty($_GET['id'])){
             
-    //         $supprimer2 = $bdd->prepare('DELETE FROM clients WHERE id_Clients =:id_Clients');
-    //         $supprimer2->bindParam(':id_Clients', $_GET['id_Clients'], 
-    //         PDO::PARAM_STR);
+            $supprimer2 = $bdd->prepare('DELETE FROM clients WHERE id_Clients =:id_Clients');
+            $supprimer2->bindParam(':id_Clients', $_GET['id'],PDO::PARAM_STR);
 
 
-    //         $supprimer2 = $supprimer2->execute();
-    //             if($supprimer2){
-    //                 echo 'votre enregistrement a bien été supprimé';
+            $supprimer2 = $supprimer2->execute();
+                if($supprimer2){
+                    echo 'votre enregistrement a bien été supprimé';
                     
                 
-    //             } else {
-    //                 echo 'Veuillez recommencer svp, une erreur est survenue';
-    //             }
-    //         }
-    // }
-    function aff_voiture() {
-        $bdd=conect();
-        $recuperation = $bdd->query('SELECT * FROM Livre');
-        while ($voit = $recuperation->fetch()) {
-        echo "<form><div> <input type='text' name='id' value='".$voit['ID_livre']."'>
-        <input type='text' name='titredulivre' value='".$voit['auteur_livre']."'>
-        <input type='text' name='annee' value='".$voit['non_du_livre_livre']."'>
-        <input type='text' name='auteurdulivre' value='".$voit['annee_livre']."'>
+                } else {
+                    echo 'Veuillez recommencer svp, une erreur est survenue';
+                }
+            }
+    }
+     function aff_voiture() {
+        $bdd=connect();
+        $recuperation = $bdd->query('SELECT * FROM vehicules');
+       
+         while ($voit = $recuperation->fetch()) {
+         echo "<form><div class='d-flex'> <input class='form-control length_crud_veh' type='text' name='id' value='".$voit['id_Vehicules']."'>
+        <input class='form-control length_crud_veh' type='text' name='type' value='".$voit['type_Vehicules']."'>
+        <input class='form-control length_crud_veh' type='text' name='modele' value='".$voit['modele_Vehicules']."'>
+     <input class='form-control length_crud_veh' type='text' name='immat' value='".$voit['immatriculation_Vehicules']."'>
         
-        <button type='submit' value='modifier2' name='action'>Modifier</button>
-        <button type='submit' value='supprimer' name='action'>Supprimer</button>
+         <button class='btn btn_jaune btn-primary' type='submit' value='modifier' name='action'>Modifier</button>
+        <button class='btn btn-danger' type='submit' value='supprimer' name='action'>Supprimer</button>
         
-        </form>
+         </form>
         
-        </div>";
+         </div>";
 
-    // }
-    // function aff_client() {
-    //     $recuperation2 = $bdd->query('SELECT * FROM Livre');
-    //     while ($clien = $recuperation2->fetch()) {
-    //     echo "<form><div> <input type='text' name='id' value='".$clien['ID_livre']."'>
-    //     <input type='text' name='titredulivre' value='".$clien['auteur_livre']."'>
-    //     <input type='text' name='annee' value='".$clien['non_du_livre_livre']."'>
-    //     <input type='text' name='auteurdulivre' value='".$clien['annee_livre']."'>
+     }
+    }
+    function aff_client() {
+        $bdd=connect();
+        $recuperation = $bdd->query('SELECT * FROM clients');
+       
+         while ($client = $recuperation->fetch()) {
+         echo "<form><div class='d-flex'> <input class='form-control length_crud_veh' type='text' name='id' value='".$client['id_Clients']."'>
+        <input class='form-control length_crud_veh' type='text' name='nom' value='".$client['Nom_Clients']."'>
+        <input class='form-control length_crud_veh' type='text' name='prenom' value='".$client['Prenom_Clients']."'>
+        <input class='form-control length_crud_veh' type='text' name='adresse' value='".$client['adresse_Clients']."'>
+        <input class='form-control length_crud_veh' type='text' name='cp' value='".$client['CP_Clients']."'>
+        <input class='form-control length_crud_veh' type='text' name='ville' value='".$client['Ville_Clients']."'>
         
-    //     <button type='submit' value='modifier2' name='action'>Modifier</button>
-    //     <button type='submit' value='supprimer' name='action'>Supprimer</button>
+         <button class='btn btn_jaune btn-primary' type='submit' value='modifier' name='action'>Modifier</button>
+        <button class='btn btn-danger' type='submit' value='supprimer' name='action'>Supprimer</button>
         
-    //     </form>
+         </form>
         
-    //     </div>";
+         </div>";
 
-//}
+     }
+    }
+    }
+    function refresh_pages($url){
+    $delai=1; 
+    header("Refresh: $delai;url=$url");
+    }
 ?>
